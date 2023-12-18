8df29d13d5de6aa3cad26e57ffc5b4166061da50
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-12-18 15:42:09 +0530

    project started

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

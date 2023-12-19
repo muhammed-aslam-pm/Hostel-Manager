b47a32044951e90820be4c7fbe6065bce9a2e3fa
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-12-19 22:22:12 +0530

    owner main screen bottom nav bar and app bar created

---
+++ lib/view/owner_home_screen/owner_home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OwnerHomeScreen extends StatelessWidget {
+  const OwnerHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}

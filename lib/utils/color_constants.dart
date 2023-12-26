6a5f665d02d0a6211523d903cd60e06c67c6cfe8
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-12-26 21:21:48 +0530

    booking form created

--- lib/utils/color_constants.dart
+++ lib/utils/color_constants.dart
@@ -10,7 +10,7 @@ class ColorConstants {
   static Color SecondaryColor2 = Color(0xffC49450);
   static Color SecondaryColor3 = Color(0xffA0B9BD);
   static Color SecondaryColor4 = Color(0x80FFC107);
-  static Color SecondaryColor5 = Color(0x8083A3AA);
+  static Color SecondaryColor5 = Color(0xff83A3AA);
   static Color colorRed = Color(0xFFFF7676);
   static Color colorGrey = Color(0xFFA4A4A4);
   static Color roomsCircleAvatarColor = Color(0x80002661);
--- lib/view/owner_booking_page/widgets/add_booking_screen.dart
+++ lib/view/owner_booking_page/widgets/add_booking_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hostel_management_app/utils/color_constants.dart';
 import 'package:hostel_management_app/utils/image_constants.dart';
@@ -22,50 +24,203 @@ class AddBookingScreen extends StatelessWidget {
           height: double.infinity,
           width: double.infinity,
           decoration: BoxDecoration(
-              color: ColorConstants.primaryColor.withOpacity(.7),
+              color: ColorConstants.SecondaryColor5,
               borderRadius: BorderRadius.circular(40)),
-          padding: EdgeInsets.all(15),
+          padding: EdgeInsets.all(25),
           child: Scaffold(
             backgroundColor: Colors.transparent,
-            body: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.arrow_back_ios_new,
-                        color: ColorConstants.primaryWhiteColor,
-                      ),
-                    ),
-                  ],
-                ),
-                CircleAvatar(
-                  radius: 25,
-                  backgroundColor: ColorConstants.primaryWhiteColor,
-                  child: Hero(
-                    tag: Image,
-                    child: Image.asset(
-                      ImageConstants.roomsIcon2,
-                      color: ColorConstants.primaryBlackColor,
+            body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back_ios_new,
+                      color: ColorConstants.primaryWhiteColor,
                     ),
                   ),
-                ),
-                Text(
-                  "Room No",
-                  style: TextStyleConstants.OwnerRoomNumber2,
-                ),
-                Text(
-                  "10",
-                  style: TextStyleConstants.OwnerRoomNumber3,
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 25,
+                            backgroundColor: ColorConstants.primaryWhiteColor,
+                            child: Hero(
+                              tag: Image,
+                              child: Image.asset(
+                                ImageConstants.roomsIcon2,
+                                color: ColorConstants.primaryBlackColor,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "Room No",
+                            style: TextStyleConstants.OwnerRoomNumber2,
+                          ),
+                          Text(
+                            "10",
+                            style: TextStyleConstants.OwnerRoomNumber3,
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Text("Bed no"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 1.5,
+                                color: ColorConstants.primaryWhiteColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 2,
+                                color: ColorConstants.primaryWhiteColor),
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text("Name "),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 1.5,
+                                color: ColorConstants.primaryWhiteColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 2,
+                                color: ColorConstants.primaryWhiteColor),
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text("Phone no"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 1.5,
+                                color: ColorConstants.primaryWhiteColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 2,
+                                color: ColorConstants.primaryWhiteColor),
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text("Joining date"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 1.5,
+                                color: ColorConstants.primaryWhiteColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                width: 2,
+                                color: ColorConstants.primaryWhiteColor),
+                          ),
+                          suffixIcon: Icon(
+                            Icons.calendar_month_outlined,
+                            color: ColorConstants.primaryWhiteColor,
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text("Advance"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      RadioMenuButton(
+                        value: true,
+                        groupValue: true,
+                        onChanged: (value) {},
+                        child: Text("Paid"),
+                      ),
+                      RadioMenuButton(
+                          value: false,
+                          groupValue: true,
+                          onChanged: (value) {},
+                          child: Text("Not Paid"))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            color: ColorConstants.primaryColor,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Center(
+                          child: Text(
+                            "Add",
+                            style: TextStyleConstants.buttonText,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),

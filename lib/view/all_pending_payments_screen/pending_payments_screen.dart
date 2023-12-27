ae5bdffcac45708a1ad209364bee52e0993c8681
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-12-27 21:24:39 +0530

    pending payments screen completed

--- lib/view/all_pending_payments_screen/pending_payments_screen.dart
+++ lib/view/all_pending_payments_screen/pending_payments_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hostel_management_app/utils/color_constants.dart';
+import 'package:hostel_management_app/utils/image_constants.dart';
 import 'package:hostel_management_app/utils/text_style_constatnts.dart';
 import 'package:hostel_management_app/view/all_pending_payments_screen/widgets/pending_payments_detailes_card.dart';
 
@@ -23,12 +26,57 @@ class PendingPaymentsScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: ListView.separated(
-            itemBuilder: (context, index) => PendingPaymentDetailesCard(),
-            separatorBuilder: (context, index) => Divider(
-                  height: 10,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListView.separated(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) => PendingPaymentDetailesCard(),
+                  separatorBuilder: (context, index) => Divider(
+                        height: 10,
+                      ),
+                  itemCount: 4),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: ColorConstants.SecondaryColor3,
+                    borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Total :",
+                      style: TextStyleConstants.upComingVaccencyText2,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                            height: 30,
+                            width: 15,
+                            child: Image.asset(
+                              ImageConstants.moneyIcon,
+                              color: ColorConstants.primaryBlackColor,
+                            )),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          "5750",
+                          style: TextStyleConstants.upComingVaccencyText2,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-            itemCount: 10),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/view/all_pending_payments_screen/widgets/pending_payments_detailes_card.dart
+++ lib/view/all_pending_payments_screen/widgets/pending_payments_detailes_card.dart
@@ -1,55 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:hostel_management_app/utils/color_constants.dart';
+import 'package:hostel_management_app/utils/image_constants.dart';
 import 'package:hostel_management_app/utils/text_style_constatnts.dart';
+import 'package:hostel_management_app/view/all_pending_payments_screen/widgets/pyment_name_card.dart';
 
 class PendingPaymentDetailesCard extends StatelessWidget {
   const PendingPaymentDetailesCard({super.key});
+
+  get roomNumber => null;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(15),
       width: double.infinity,
-      decoration: BoxDecoration(color: ColorConstants.SecondaryColor1),
+      decoration: BoxDecoration(
+          color: ColorConstants.SecondaryColor3,
+          borderRadius: BorderRadius.circular(15)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Room no 1"),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: ColorConstants.SecondaryColor1),
+            padding: EdgeInsets.symmetric(horizontal: 7, vertical: 4),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  ImageConstants.roomsIcon2,
+                  color: ColorConstants.primaryBlackColor,
+                  height: 25,
+                  width: 25,
+                ),
+                Text(
+                  "Room No ",
+                  style: TextStyleConstants.bookingsRoomNumber,
+                ),
+                Text(
+                  "01",
+                  style: TextStyleConstants.bookingsRoomNumber,
+                )
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(top: 10, left: 10),
             child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 15,
-                            child: Icon(
-                              Icons.person,
-                              color: ColorConstants.primaryWhiteColor,
-                            ),
-                            backgroundColor: ColorConstants.SecondaryColor4,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            "Aslam",
-                            style: TextStyleConstants.dashboardBookingName,
-                          ),
-                        ],
-                      ),
-                      Text("5750")
-                    ],
-                  ),
-                )
-              ],
+              children: List.generate(4, (index) => PaymentNameCard()),
             ),
           )
         ],
